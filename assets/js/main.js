7a0e990fe1476e79f448fb948613031e595866cb
George Katsampu <g.katsiampas@skroutz.gr>
2021-07-03 16:57:32 +0300

    Add animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,61 @@ const onScroll = () => {
 }
 
 window.addEventListener('scroll', onScroll)
+
+
+///////////////
+// Scroll Magic
+
+gsap.registerPlugin(ScrollTrigger);
+
+// 1st step
+gsap.to("#mpoukali-animated", {
+  x: 0,
+  y: 1648,
+  rotation: 327,
+  scrollTrigger: {
+    trigger: "#start",
+    start: "80px",
+    end: "+=700px",
+    scrub: true,
+    id: "scrub"
+  }
+});
+
+// 2nd step
+gsap.fromTo("#mpoukali-animated", {
+  x: 0,
+  y: 1648,
+  rotation: 327,
+},
+{
+  x: 101,
+  y: 2718,
+  rotation: 360,
+  scrollTrigger: {
+    trigger: "#s3",
+    start: "top center",
+    end: "top 100px",
+    scrub: true,
+    id: "scrub"
+  }
+});
+
+// 3rd step
+gsap.fromTo("#mpoukali-animated", {
+  x: 101,
+  y: 2718,
+  rotation: 360,
+},
+{
+  x: 0,
+  y: 3631,
+  rotation: 374,
+  scrollTrigger: {
+    trigger: "#s4",
+    start: "top center",
+    end: "top 100px",
+    scrub: true,
+    id: "scrub"
+  }
+});
