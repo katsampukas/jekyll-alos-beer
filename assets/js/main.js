0fc016b7dfe734faf11c16ec5cb47adcb2fc7ac1
George Katsampu <g.katsiampas@skroutz.gr>
2021-07-10 15:23:11 +0300

    Add menu functionality and fix bottle movements

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,35 @@ const onScroll = () => {
   }
 }
 
-window.addEventListener('scroll', onScroll)
+window.addEventListener('scroll', onScroll);
+
+
+// Menu
+
+const menu = document.getElementById('nav-trigger');
+const body = document.body;
+
+menu.addEventListener('click', function(){
+
+  if (menu.checked == true){
+    body.classList.add("menu-active");
+  }
+  else {
+    body.classList.remove("menu-active");
+  }
+})
+
+var menu_items = document.getElementsByClassName("menu-anchor");
+
+Array.from(menu_items).forEach(function(element) {
+  element.addEventListener('click', function(e){
+    e.preventDefault();
+    document.getElementById('nav-trigger').click();
+    var id = element.href.split("/").pop().replace("#", "");;
+    var scroll = document.getElementById(id);
+    scroll.scrollIntoView({behavior: 'smooth' })
+  });
+});
 
 
 ///////////////
@@ -24,13 +52,13 @@ window.addEventListener('scroll', onScroll)
 
 gsap.registerPlugin(ScrollTrigger);
 
-if (window,innerWidth < 400) {
+if (window,innerWidth < 600) {
 
   // 1st step
   gsap.to("#mpoukali-animated", {
     x: 0,
-    y: 1648,
-    rotation: 327,
+    y: 1432,
+    rotation: 324,
     scrollTrigger: {
       trigger: "#start",
       start: "80px",
@@ -43,12 +71,12 @@ if (window,innerWidth < 400) {
   // 2nd step
   gsap.fromTo("#mpoukali-animated", {
     x: 0,
-    y: 1648,
-    rotation: 327,
+    y: 1432,
+    rotation: 324,
   },
   {
     x: 87,
-    y: 2541,
+    y: 2354,
     rotation: 360,
     scrollTrigger: {
       trigger: "#s3",
@@ -62,12 +90,12 @@ if (window,innerWidth < 400) {
   // 3rd step
   gsap.fromTo("#mpoukali-animated", {
     x: 87,
-    y: 2541,
+    y: 2354,
     rotation: 360,
   },
   {
     x: 0,
-    y: 3320,
+    y: 3175,
     rotation: 386,
     scrollTrigger: {
       trigger: "#s4",
